04bf2b5d41e80f86fcbaebd0f67bba4670f3673b
LuisGonzalez27 <luis27coding@gmail.com>
2022-11-21 11:48:00 +0100

    Scaffolding start

---
+++ src/components/appHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="test">
+        Test
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as*;
+
+.test {
+    color: $white;
+}
+</style>
